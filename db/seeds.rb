ac2fff2f1901eb25a4570e19b7aa08449d46b5ce
epittab <ep7m@me.com>
2020-05-17 15:04:53 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: "123", store: "Tysons Corner")
+Coupon.create(coupon_code: "234", store: "Reston Town Center")
+Coupon.create(coupon_code: "345", store: "Fair Oaks")
+Coupon.create(coupon_code: "456", store: "Georgetown Mall")
